4d60318a494bec6b96ee3422eca32d4758ea49e4
giuliarc <giuliaribeiro26@gmail.com>
2024-07-08 21:57:37 -0300

    so pra trocar de branch

--- src/model.py
+++ src/model.py
@@ -11,4 +11,10 @@ class OrderLine:
 
 class Batch:
     def __init__(self, ref: str, sku:str, qty: int, eta: Optional[date]):
-        
+      self.reference = ref
+      self.sku = sku
+      self.eta = eta
+      self.available_quantity = qty
+
+    def allocate(self, line:OrderLine)
+       self.available_quantity -= line.qty
